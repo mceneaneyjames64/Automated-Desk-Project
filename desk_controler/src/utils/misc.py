c80759a854d9f8776364f9eaaf1ad99df64916ae
James McEneaney <mceneaneyjames64@gmail.com>
2026-03-24 08:56:56 -0400

    Update misc.py
    added comments

--- desk_controler/src/utils/misc.py
+++ desk_controler/src/utils/misc.py
@@ -1,6 +1,7 @@
 from math import atan2, degrees
 
 def vector_to_degrees(x, y):
+	"""convert coordinates into an angle in degrees"""
     angle = degrees(atan2(y, x))
     if angle <0:
 	    angle += 360
