e693c54b5ea7310c5b91fb86bea6e5fe4b6566bd
Nicholas Ligday <nicksl2022@gmail.com>
2026-04-15 00:22:45 -0400

    updated motor move

--- desk_controler/src/utils/__init__.py
+++ desk_controler/src/utils/__init__.py
@@ -1,3 +1,3 @@
 from .timeout import timeout, TimeoutError
-from .misc import vector_to_degrees
-__all__ = ['timeout', 'TimeoutError', 'vector_to_degrees']
+from .misc import vector_to_degrees, z_axis_to_degrees
+__all__ = ['timeout', 'TimeoutError', 'vector_to_degrees', 'z_axis_to_degrees']
